28a1a0d37a6891116e74c69a9f930a4e1c4de78a
alif bagas saputra <alifbagas89@gmail.com>
2024-05-13 23:54:01 +0700

    tugas 10

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,50 @@
 <template>
   <div id="app">
+    <button @click="toggleData('todos')">Todos</button>
+    <button @click="toggleData('posts')">Posts</button>
     <!-- Ganti konten yang ada dengan komponen Kegiatan -->
-    <Kegiatan />
+    <Kegiatan :data="currentData" />
   </div>
 </template>
 
 <script>
-// Impor komponen Kegiatan.vue
-import Kegiatan from './components/Kegiatan.vue'
+import Kegiatan from './components/Kegiatan.vue';
 
 export default {
   name: 'App',
   components: {
-    // Tambahkan komponen Kegiatan ke dalam daftar komponen
     Kegiatan
+  },
+  data() {
+    return {
+      todos: [],
+      posts: [],
+      currentData: [] // Menyimpan data yang sedang ditampilkan
+    };
+  },
+  created() {
+    this.fetchData('todos'); // Mulai dengan menampilkan data todos saat aplikasi dimuat
+  },
+  methods: {
+    fetchData(type) {
+      fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(response => response.json())
+        .then(data => {
+          this[type] = data;
+          if (type === 'todos') {
+            this.currentData = this.todos;
+          } else {
+            this.currentData = this.posts;
+          }
+        })
+        .catch(error => {
+          console.error(`Error fetching ${type}:`, error);
+        });
+    },
+    toggleData(type) {
+      this.currentData = []; // Kosongkan data yang ditampilkan
+      this.fetchData(type); // Ambil data baru berdasarkan jenisnya
+    }
   }
 }
 </script>
-
-<style>
-/* Gaya untuk aplikasi Vue Anda */
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
